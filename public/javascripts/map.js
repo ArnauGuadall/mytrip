cda49d08a3e7f7dddb38143683a151732ca286fb
Arnau Guadall <arnau.guadall@gmail.com>
2017-07-17 16:09:15 +0200

    autocomplete and refactoring maps

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,125 +1,114 @@
 // Google Maps API
 
-var geocoder = new google.maps.Geocoder();
-var map;
-var directionsDisplay;
-var directionsService = new google.maps.DirectionsService();
 var locations = [];
+var wayPoints = [];
 var orderCount = 0;
+var geocoder;
+var map;
 
-// IronhackBCN Location by default 
-var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 10,  
-  center: new google.maps.LatLng(41.3977381, 2.090471916),
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-});
+function addingWayPoints() {
 
-function initialize() {  
+  if (locations.length > 0) {
+     wayPoints = []
 
-  directionsDisplay = new google.maps.DirectionsRenderer();
-
-  directionsDisplay.setMap(map);
-
-  var infowindow = new google.maps.InfoWindow();
-  var marker, i;
-  var request = {
-    travelMode: google.maps.TravelMode.DRIVING
-  };
-
-  console.log("location length is.. " + locations.length);
-
-  if (locations.length > 0){
-    // Go though the array and place a marker on the map base on location array
     for (i = 0; i < locations.length; i++) {
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
-
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-
-      if (i == 0) request.origin = marker.getPosition();
-      else if (i == locations.length - 1) request.destination = marker.getPosition();
-      else {
-        if (!request.waypoints) request.waypoints = [];
-        request.waypoints.push({
-          location: marker.getPosition(),
+      if(i > 0 && i < locations.length){
+        // create all the points in between from origin to ending
+        wayPoints.push({
+          location: locations[i].address,
           stopover: true
         });
       }
     }
-
-    directionsService.route(request, function(result, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(result);
-      }
-    });
-    // End
-  } else {
-
-    if (navigator.geolocation) {
-
-      navigator.geolocation.getCurrentPosition(function (position) {
-
-        center = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        
-        map.setCenter(center);
-        
-      }, function () {
-        console.log('Error in the geolocation service.');
-      });
-    } else {
-      console.log('Browser does not support geolocation.');
-    }
   }
+
+  if (locations.length > 1) {
+
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+    var directionsService = new google.maps.DirectionsService();
     
+    // get the configuration of the route and display it on the map
+    calculateAndDisplayRoute(directionsService, directionsDisplay)
+  }
+
 }
 
-// waits until the user clicks the button and execute the geocodeAddres function
-document.getElementById('submit').addEventListener('click', function() {
-    geocodeAddress(geocoder, map);
-  }); 
+function initialize() {
 
-// GEOCODE Get the input from address and find a place with that name and push it to an array
+  geocoder = new google.maps.Geocoder();
+
+  var input = document.getElementById("address")
+  var autocomplete = new google.maps.places.Autocomplete(input)
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: new google.maps.LatLng(41.3977381, 2.090471916),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  // waits until the user clis the button and execute the geocodeAddres function
+  document.getElementById('submit').addEventListener('click', function () {
+    geocodeAddress(geocoder, map);
+  });
+
+  // GEOCODE Get the input from address and find a place with that name and push it to an array
   function geocodeAddress(geocoder, resultsMap) {
-    
-	  var address = document.getElementById('address').value;
-	  
-	  geocoder.geocode({'address': address}, function(results, status) {
-	    if (status === 'OK') {
-	      resultsMap.setCenter(results[0].geometry.location);
-	      var marker = new google.maps.Marker({
-	        map: resultsMap,
-	        position: results[0].geometry.location
-        });        
+
+    var address = document.getElementById('address').value;
+
+    geocoder.geocode({ 'address': address }, (results, status) => {
+      if (status === 'OK') {
+
+        resultsMap.setCenter(results[0].geometry.location);
+
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
 
         var lat = results[0].geometry.location.lat()
         var lng = results[0].geometry.location.lng()
 
         locations.push(
-          [address, lat, lng, orderCount]
+          { address, lat, lng, orderCount }
         );
+        
+        //adding waypoints to the route
+        addingWayPoints()
 
         orderCount++;
-        console.log("The orderCount is: " + orderCount);
-    
-	    } else {
-	      alert('Geocode was not successful for the following reason: ' + status);
+
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
       }
-          
-      initialize();
+
     });
-      
-    console.log(locations);
-  } 
-  //End GEOCODE
-  
-google.maps.event.addDomListener(window, 'load', initialize);
+
+  }
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  directionsDisplay.setMap(map);
+
+  directionsService.route({
+    origin: { lat: locations[0].lat, lng: locations[0].lng },
+    destination: { lat: locations[locations.length - 1].lat, lng: locations[locations.length - 1].lng },
+    waypoints: wayPoints,
+    travelMode: google.maps.TravelMode.DRIVING
+  }, function (response, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
+
+$(document).ready(function () {
+  initialize()
+})
